18830e2c607e37db78859ea8dfeaf3745245accb
Son <iresonardiente7@gmail.com>
2025-06-23 03:40:11 +0000

    finished task2.java

---
+++ task2.java
@@ -0,0 +1,18 @@
+public class task2{
+    public static void main(String[] args){
+        //  H3110 w0rld 2.0 true
+
+        short hello = (short)(3110);
+        byte world = 0;
+        double num = 2.0;
+        boolean t  = true;
+
+        System.out.println("H"+ hello + " w" + world + "rld" +" " + num +" " + t);
+
+
+
+
+    }
+
+
+}
