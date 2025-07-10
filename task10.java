6eaa0120fbc08bae18c8b1ca9cf2eb6427769c00
Son <iresonardiente7@gmail.com>
2025-07-10 03:24:03 +0000

    Finished task 10

---
+++ task10.java
@@ -0,0 +1,33 @@
+public class task10 {
+
+    public static void main(String []a){
+        Student [] students= new Student []{
+            new Student("Morgan", "Freeman"),
+            new Student("Bradd", "Pitt"),
+            new Student("Kevin","Spacey"),
+        };
+        for(Student s : students) {
+            s.printFullname();
+        }
+        }
+
+    }
+
+
+    class Student  { 
+        private String firstName;
+        private String lastName;
+        public Student(String firstName, String lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public void printFullname(){
+            System.out.println(firstName + " " + lastName);
+        }
+    }
+
+
+
+
+
+ 
